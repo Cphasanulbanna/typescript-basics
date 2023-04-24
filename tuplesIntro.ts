a3639320e6eae6bada11bb18a9804c5de7c9bd33
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-04-24 14:44:48 +0530

    completed tuples

---
+++ tuplesIntro.ts
@@ -0,0 +1,8 @@
+//tuple syntax
+//ordering data in specific type order
+const user: [string, number, boolean] = ["banna", 10, true];
+
+//another example
+let rgb: [number, number, number] = [255, 255, 255];
+
+export {};
