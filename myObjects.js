12ca0b497eed2dbd6608d520b54cc342f8b67a90
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-04-24 11:50:09 +0530

    topic objects covered

---
+++ myObjects.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// creating userdata
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({ name: "banna", isPaid: false });
---
+++ myObjects.ts
@@ -0,0 +1,16 @@
+// creating userdata
+function createUser({ name: string, isPaid: boolean }) {}
+
+createUser({ name: "banna", isPaid: false });
+
+//function that should return object
+function getCourse(): {} {
+    return {};
+}
+
+//function that should return object with specific types and properties
+function getInfo(): { name: string; price: number } {
+    return { name: "banna", price: 100 };
+}
+
+export {};
